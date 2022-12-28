b6e52c8b826c07b275302010ad10350e1c658961
K Khutorenko <k.khutorenko@gmail.com>
2022-12-28 16:04:22 +0300

    models added

--- menuapp/admin.py
+++ menuapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MenuModel, MenuItem
 
-# Register your models here.
+
+@admin.register(MenuItem, MenuModel)
+class DefaultAdmin(admin.ModelAdmin):
+    pass
--- menuapp/models.py
+++ menuapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuModel(models.Model):
+    title = models.CharField(max_length=255, unique=True,
+                             verbose_name='Название меню')
+    slug = models.SlugField(max_length=255, unique=True,
+                            verbose_name='URL')
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название подменю')
+    slug = models.SlugField(max_length=255, verbose_name='URL')
+    menu = models.ForeignKey(
+        MenuModel, related_name='menutems', on_delete=models.CASCADE)
+    parent = models.ForeignKey(
+        'self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
